ed792030ff3ba1f5bf218e8ad60dd93c34702028
Airaksinen Manu <mairaksi@t40521-ubu01.org.aalto.fi>
2016-10-05 17:21:22 +0300

    Foo test

--- src/glott/Analysis.cpp
+++ src/glott/Analysis.cpp
@@ -1,49 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Tuomo Raitio
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *
- *
- *
- * <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
- *              GlottHMM Speech Parameter Extractor
- * <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
- *
- * This program reads a speech file and extracts speech
- * parameters using glottal inverse filtering.
- *
- * This program has been written in Aalto University,
- * Department of Signal Processign and Acoustics, Espoo, Finland
- *
- * Authors: Tuomo Raitio (2008-2015), Lauri Juvela (2015-)
- * Acknowledgements: Antti Suni, Paavo Alku, Martti Vainio
- *
- * File Analysis.c
- * Version: 2.0
- *
- */
-
-
-
 /***********************************************/
 /*                 INCLUDE                     */
 /***********************************************/
@@ -74,11 +28,6 @@
 #include "DebugUtils.h"
 
 
-using namespace std;
-
-
-
-
 /*******************************************************************/
 /*                          MAIN                                   */
 /*******************************************************************/
@@ -99,7 +48,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	//double foo[] = {1,2,3};
-	//std::cout << sizeof(foo) / sizeof(double) << std::endl;
+    std::cout << "Foo" << std::endl;
 
 	//create_file(fname, SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
 	/* Read sound file and allocate data */
@@ -112,14 +61,15 @@ int main(int argc, char *argv[]) {
 
 	GetGci(params, data.signal, &(data.gci_inds));
 
+    std::cout << "Foo" << std::endl;
 
-	//VPrint1(data.gci_signal);
 	GetGain(params, data.signal, &(data.frame_energy));
 
 	SpectralAnalysis(params, data, &(data.poly_vocal_tract));
 
 	// Process VT parameters?
 
+    std::cout << "Foo" << std::endl;
 
 
 
