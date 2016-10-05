40d446da14632f30afaf669c4bdf097a376c6f5f
Airaksinen Manu <mairaksi@t40521-ubu01.org.aalto.fi>
2016-10-05 17:03:46 +0300

    Testi

--- src/glott/InverseFiltering.cpp
+++ src/glott/InverseFiltering.cpp
@@ -107,11 +107,143 @@ void GetLpWeight(const Param &params, const LpWeightingFunction &weight_type,
 	}
 }
 
-void WWLP(const gsl::vector &weight_function, const double &warping_lambda_vt, const int &lpc_order_vt, const gsl::vector &frame, gsl::vector *A) {
 
-	//
 
-}
+/**
+ * Function WWLP
+ *
+ * Calculate Warped Weighted Linear Prediction (WWLP) coefficients using
+ * autocorrelation method.
+ *
+ * @param frame pointer to the samples
+ * @param a pointer to coefficiets (length p = LP degree)
+ * @param Wn pointer to WWLP weighting function
+ * @param lambda warping coefficient
+ */
+
+/*
+void WWLP(const gsl::vector &weight_function, const double &warping_lambda_vt,
+		const int &lpc_order_vt, const gsl::vector &frame, gsl::vector *A) {
+
+
+
+
+	int i,j,k,s,p = a->size-1;
+	double win = 0.0,sum = 0.0;
+	gsl::vector wframe(frame->size);
+	gsl_vector *a_temp = gsl_vector_calloc(p);
+	gsl_vector *r = gsl_vector_calloc(p+1);
+	gsl_vector *b = gsl_vector_alloc(p);
+	gsl_matrix *R = gsl_matrix_alloc (p, p);
+	gsl_permutation *perm = gsl_permutation_alloc(p);
+
+
+
+	// Copy warped frame
+	gsl_vector *wframe_w = gsl_vector_calloc(wframe->size+p);
+
+	for(i=0;i<wframe->size;i++)
+		gsl_vector_set(wframe_w,i,gsl_vector_get(wframe,i));
+
+	//for(i=0;i<wframe->size;i++)
+	//	gsl_vector_set(wframe_w,i,gsl_vector_get(wframe,i));
+
+
+
+	gsl_matrix *Y = gsl_matrix_calloc(p+1,wframe->size+p); // Delayed and weighted versions of the signal
+	gsl_matrix *Rfull = gsl_matrix_calloc(p+1,p+1);
+
+	// Matrix Dw
+	for(j=0;j<wframe->size;j++) {
+		if(Wn == NULL) {
+			gsl_matrix_set(Y,0,j,gsl_vector_get(wframe,j));
+		} else {
+			gsl_matrix_set(Y,0,j,sqrt(gsl_vector_get(Wn,j))*gsl_vector_get(wframe,j));
+		}
+
+	}
+
+	for(i=1;i<p+1;i++) {
+		AllPassDelay2(wframe_w, lambda);
+		for(j=0;j<wframe_w->size;j++) {
+			if(Wn == NULL) {
+				gsl_matrix_set(Y,i,j,gsl_vector_get(wframe_w,j));
+			} else {
+				gsl_matrix_set(Y,i,j,sqrt(gsl_vector_get(Wn,j))*gsl_vector_get(wframe_w,j));
+			}
+		}
+
+
+	}
+
+
+
+	//printf("%i, %i\n",Y->size1,Y->size2);
+
+	// Rfull = Dw*Dw'
+	gsl_blas_dgemm(CblasNoTrans,CblasTrans,1.0,Y,Y,0.0,Rfull);
+
+	//MPrint1(Rfull);
+
+	// Autocorrelation matrix R (R = (YT*Y)/N, size p*p) and vector b (size p)
+
+	for(i=0;i<p;i++) {
+		for(j=0;j<p;j++) {
+			gsl_matrix_set(R,i,j,gsl_matrix_get(Rfull,i+1,j+1));
+		}
+		gsl_vector_set(b,i,gsl_matrix_get(Rfull,i+1,0));
+		sum += gsl_vector_get(b,i);
+	}
+
+
+	//MPrint2(R);
+	//VPrint1(b);
+
+	//Ra=r solver (LU-decomposition) (Do not evaluate LU if sum = 0)
+
+	if(sum != 0) {
+		gsl_linalg_LU_decomp(R, perm, &s);
+		gsl_linalg_LU_solve(R, perm, b, a_temp);
+	}
+
+	// Set LP-coefficients to vector "a"
+	for(i=1; i<a->size; i++) {
+		gsl_vector_set(a, i, (-1)*gsl_vector_get(a_temp, i-1));
+	}
+	gsl_vector_set(a, 0, 1);
+
+	//VPrint2(a);
+	// Stabilize unstable filter by scaling the poles along the unit circle
+	AC_stabilize(a,frame->size);
+
+	//Pole_stabilize(a);
+
+	// Remove real roots
+	if(ROOT_SCALING == 1) {
+		if(a->size > ROOT_SCALE_MIN_DEGREE) {
+			RealRootScale(a);
+		}
+	}
+
+	// Replace NaN-values with zeros in case of all-zero frames
+	for(i=0;i<a->size;i++) {
+		if(gsl_isnan(gsl_vector_get(a,i)))
+			gsl_vector_set(a,i,0);
+	}
+
+	// Free memory
+	gsl_vector_free(wframe);
+	gsl_vector_free(wframe_w);
+	gsl_vector_free(a_temp);
+	gsl_vector_free(r);
+	gsl_vector_free(b);
+	gsl_matrix_free(R);
+	gsl_matrix_free(Rfull);
+	gsl_matrix_free(Y);
+	gsl_permutation_free(perm);
+
+
+}*/
 
 void LPC(const gsl::vector &frame, const int &lpc_order, gsl::vector *A) {
 	gsl::vector r;
