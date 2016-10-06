c13c39accdc6941c2c836c77cc6bad339b7224fb
ljuvela <lauri.juvela@aalto.fi>
2016-10-06 12:19:44 +0300

    added test script "test.sh"

--- src/glott/Analysis.cpp
+++ src/glott/Analysis.cpp
@@ -34,40 +34,39 @@
 
 int main(int argc, char *argv[]) {
 
+   const char *wav_filename = argv[1];
+   const char *default_config_filename = argv[2];
+   const char *user_config_filename = argv[3];
 
-	const char *wav_filename = argv[1];
-	const char *default_config_filename = argv[2];
-	const char *user_config_filename = argv[3];
+   Param params;
+   if (ReadConfig(default_config_filename, true, &params) == EXIT_FAILURE)
+      return EXIT_FAILURE;
+   if (argc > 3) {
+      if (ReadConfig(user_config_filename, false, &params) == EXIT_FAILURE)
+         return EXIT_FAILURE;
+   }
 
-	Param params;
-	if (ReadConfig(default_config_filename, true, &params) == EXIT_FAILURE)
-		return EXIT_FAILURE;
-	if (argc > 3) {
-		if (ReadConfig(user_config_filename, false, &params) == EXIT_FAILURE)
-			return EXIT_FAILURE;
-	}
+   //create_file(fname, SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
+   /* Read sound file and allocate data */
+   AnalysisData data;
+   ReadFile(wav_filename, &(data.signal), &params);
+   data.AllocateData(params);
 
-	//create_file(fname, SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
-	/* Read sound file and allocate data */
-	AnalysisData data;
-	ReadFile(wav_filename, &(data.signal), &params);
-	data.AllocateData(params);
+   /* F0 Analysis */
+   GetF0(params, data.signal, &(data.fundf));
 
-	/* F0 Analysis */
-	GetF0(params, data.signal, &(data.fundf));
-
-	GetGci(params, data.signal, &(data.gci_inds));
+   GetGci(params, data.signal, &(data.gci_inds));
 
 
-	GetGain(params, data.signal, &(data.frame_energy));
+   GetGain(params, data.signal, &(data.frame_energy));
 
-	SpectralAnalysis(params, data, &(data.poly_vocal_tract));
+   SpectralAnalysis(params, data, &(data.poly_vocal_tract));
 
-	// Process VT parameters?
+   // Process VT parameters?
 
-	/* Finish */
-	printf("Finished analysis.\n\n");
-	return EXIT_SUCCESS;
+   /* Finish */
+   printf("Finished analysis.\n\n");
+   return EXIT_SUCCESS;
 }
 
 /***********/
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# run analysis program
+./src/Analysis ./wav/arctic_fe.wav ./config/config_default.cfg
