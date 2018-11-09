11650d8ff010675c9bd9a86cafb759eb621b14d7
ljuvela <lauri.juvela@aalto.fi>
2018-11-09 14:18:00 +0200

    added dnn_demo/get_data.sh to index

---
+++ dnn_demo/get_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATADIR='./dnn_demo/data/wav'
+mkdir -p $DATADIR
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0001.wav -O $DATADIR/slt_arctic_a0001.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0002.wav -O $DATADIR/slt_arctic_a0002.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0003.wav -O $DATADIR/slt_arctic_a0003.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0004.wav -O $DATADIR/slt_arctic_a0004.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0005.wav -O $DATADIR/slt_arctic_a0005.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0006.wav -O $DATADIR/slt_arctic_a0006.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0007.wav -O $DATADIR/slt_arctic_a0007.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0008.wav -O $DATADIR/slt_arctic_a0008.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0009.wav -O $DATADIR/slt_arctic_a0009.wav
+wget http://festvox.org/cmu_arctic/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0010.wav -O $DATADIR/slt_arctic_a0010.wav
