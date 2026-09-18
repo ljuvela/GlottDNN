c84d628b897f8658f3f436315b5405b69274b878
Lauri Juvela <lauri.juvela@aalto.fi>
2026-09-18 11:14:49 +0300

    auto download test sample if missing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from pathlib import Path
+from urllib.request import urlopen
+
+import pytest
 
 
 ROOT = Path(__file__).resolve().parents[1]
@@ -12,3 +15,16 @@ if conda_prefix:
     library_path = str(Path(conda_prefix) / "lib")
     variable = "DYLD_LIBRARY_PATH" if sys.platform == "darwin" else "LD_LIBRARY_PATH"
     os.environ[variable] = library_path + os.pathsep + os.environ.get(variable, "")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def arctic_sample():
+    sample = ROOT / "data" / "tmp" / "slt_arctic_a0001.wav"
+    if not sample.exists():
+        sample.parent.mkdir(parents=True, exist_ok=True)
+        url = (
+            "http://festvox.org/cmu_arctic/cmu_arctic/"
+            "cmu_us_slt_arctic/wav/arctic_a0001.wav"
+        )
+        with urlopen(url) as response, sample.open("wb") as output:
+            output.write(response.read())
